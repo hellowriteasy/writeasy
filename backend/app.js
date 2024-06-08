a743517f278864481f58d93397d9d4ccc344e410
skilledsantosh <santoshkunwar@skillprompt.com>
2024-06-08 10:33:05 +0545

    used 5000 port for backend

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ app.get("/success", (req, res) => {
 // Global error handling
 app.use(errorHandlingMiddleware);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
