3bd0948bb46303b60b87d1e1ee8ec45e4629d8e6
skilledsantosh <santoshkunwar@skillprompt.com>
2024-07-11 20:16:09 +0545

    top writing displayed :

--- frontend/app/components/Others/TopWriting.tsx
+++ frontend/app/components/Others/TopWriting.tsx
@@ -1,52 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Image from 'next/image';
-import Crown from '@/public/Game/Crown.svg';
-import Link from 'next/link';
-import { axiosInstance } from '@/app/utils/config/axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Image from "next/image";
+import Crown from "@/public/Game/Crown.svg";
+import Link from "next/link";
+import { axiosInstance } from "@/app/utils/config/axios";
+import { TUser } from "@/app/utils/types";
 interface Story {
   _id: string;
   title: string;
   content: string;
   wordCount: number;
+  contest: string;
+  prompt: string;
+  user: TUser;
 }
 
 const TopWriting: React.FC = () => {
   const [topStories, setTopStories] = useState<Story[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const AxiosIns=axiosInstance("")
+  const AxiosIns = axiosInstance("");
   useEffect(() => {
-    AxiosIns.get('/stories/top')
-      .then(response => {
-        setTopStories(response.data);
+    AxiosIns.get("/stories/contest/previous-week-top-stories")
+      .then((response) => {
+        setTopStories(response.data.data);
         setIsLoading(false);
       })
-      .catch(error => {
+      .catch((error) => {
         setError("There was an error fetching the top stories.");
         setIsLoading(false);
       });
   }, []);
-
+  console.log("debug", topStories);
   return (
     <div className="w-96 mid:w-[200px]   h-auto flex justify-center relative yellow border-4 border-yellow-500 rounded-3xl p-4 md:p-0">
       <div className="absolute -top-6 sm-hide md:-top-9 right-4 md:right-[-12px]">
-        <Image src={Crown} alt="Crown" width={40} height={40} className="md:w-auto md:h-auto" />
+        <Image
+          src={Crown}
+          alt="Crown"
+          width={40}
+          height={40}
+          className="md:w-auto md:h-auto"
+        />
       </div>
 
       <div className="text-center w-full md:w-11/12 pt-4 text-xl md:text-3xl font-bold">
-        <h2 className="font-comic text-[2vw] py-4 font-bold">Top writings of previous week</h2>
+        <h2 className="font-comic text-[2vw] py-4 font-bold leading-normal">
+          Top writings of previous week
+        </h2>
         {isLoading ? (
           <p>Loading...</p>
         ) : error ? (
           <p>{error}</p>
         ) : (
-          <ul className='p-4'>
-            {topStories.map(story => (
-              <li  key={story._id} className="my-4 ">
-                <h3 className="text-lg  font-comic md:text-xl font-bold">{story.title}</h3>
-              
-              </li>
+          <ul className="p-4 w-full flex flex-col gap-y-1">
+            {topStories.map((story, index) => (
+              <Link
+                href={`/Contests/${story.contest}/${story.prompt}`}
+                key={story._id}
+                className="my-2 flex flex-col  items-start"
+              >
+                <h1 className="text-xl text-start font-comic font-bold capitalize ">
+                  {story.user.username}
+                </h1>
+                <h3 className="text-lg text-start font-comic md:text-xl font-normal line-clamp-1">
+                  {story.content}
+                </h3>
+              </Link>
             ))}
           </ul>
         )}
