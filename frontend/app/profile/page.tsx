040fae319165fd75927aec310cae753762861206
skilledsantosh <santoshkunwar@skillprompt.com>
2024-07-16 22:03:58 +0545

    updated subscription price

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,20 +1,19 @@
-'use client'
-import React, { useState, useEffect } from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 import UserStory from "@/app/components/profile/UserStory";
-import ReactPaginate from 'react-paginate';
+import ReactPaginate from "react-paginate";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
-import useAuthStore from '../store/useAuthStore';
-import { TStory } from '../utils/types';
-import NotFound from '../components/Others/NotFound';
-import ProfileTabs from '../components/profile/ProfileTabs';
-import { axiosInstance } from '../utils/config/axios';
-
+import useAuthStore from "../store/useAuthStore";
+import { TStory } from "../utils/types";
+import NotFound from "../components/Others/NotFound";
+import ProfileTabs from "../components/profile/ProfileTabs";
+import { axiosInstance } from "../utils/config/axios";
 
 const Page: React.FC = () => {
   const itemsPerPage = 5;
   const [currentPage, setCurrentPage] = useState(1);
   const [userStories, setUserStories] = useState<TStory[]>([]);
-  const [error, setError] = useState<string|null>(null);
+  const [error, setError] = useState<string | null>(null);
   const [selectedOption, setSelectedOption] = useState<string>("");
   const [pageDetails, setPageDetails] = useState({
     page: 1,
@@ -23,60 +22,52 @@ const Page: React.FC = () => {
   });
   const { userId } = useAuthStore();
 
- const AxiosIns=axiosInstance("")
-  
-    const fetchUserStories = async (page = 1, perPage = itemsPerPage) => {
+  const AxiosIns = axiosInstance("");
+
+  const fetchUserStories = async (page = 1, perPage = itemsPerPage) => {
+    try {
+      const response = await AxiosIns.get("/stories/user", {
+        params: {
+          userId: userId,
+          storyType: "practice",
+          page,
+        },
+      });
+
+      setUserStories(response.data?.data.reverse());
+      setPageDetails(response.data.pageData);
+    } catch (error: any) {
+      setError("Error fetching user stories: " + error.message);
+    }
+  };
+  useEffect(() => {
+    fetchUserStories(currentPage, itemsPerPage);
+  }, [currentPage]);
+
+  async function onSuccess() {
+    const fetchUserStories = async () => {
       try {
-        const response = await AxiosIns.get('/stories/user', {
+        const response = await AxiosIns.get("/stories/user", {
           params: {
-            userId:userId,
-            storyType: 'practice',
-            page
-          }
+            userId: userId,
+            storyType: "practice",
+          },
         });
-       
+
         setUserStories(response.data?.data.reverse());
-        setPageDetails(response.data.pageData);
-      } catch (error:any) {
-        setError('Error fetching user stories: ' + error.message);
-       
+      } catch (error: any) {
+        setError("Error fetching user stories: " + error.message);
       }
     };
-    useEffect(() => {
-      fetchUserStories(currentPage, itemsPerPage);
-    }, [currentPage]);
 
-   async function  onSuccess(){
- 
-      const fetchUserStories = async () => {
-        try {
-          const response = await AxiosIns.get<TStory[]>('/stories/user', {
-            params: {
-              userId:userId,
-              storyType: 'practice',
-              
-            }
-          });
-         
-          setUserStories(response.data.reverse());
-          
-        } catch (error:any) {
-          setError('Error fetching user stories: ' + error.message);
-         
-        }
-      };
-  
-      fetchUserStories();
-   
+    fetchUserStories();
   }
-  
+
   const handlePageClick = (event: { selected: number }) => {
     setCurrentPage(event.selected + 1);
   };
 
   const offset = currentPage * itemsPerPage;
-  
-
 
   if (error) return <p>{error}</p>;
 
@@ -84,48 +75,52 @@ const Page: React.FC = () => {
     <div className="font-poppins">
       <ProfileTabs />
       <div className="flex flex-col items-center gap-10 min-h-[500px]">
-        { userStories.length > 0? userStories.map((story) => (
-          <UserStory
-            key={story._id}
-            title={story.title}
-            contestTitle={story.contest?.contestTheme || ""}
-            promptTitle={story.prompt?.title}
-            prompt_id={story.prompt?._id}
-            description={story.content}
-            id={story._id}
-            corrections={story.corrections}
-            type={story.storyType}
-            contributors={story.contributors}
-            onsuccess={onSuccess}
-          />
-        )) :<NotFound text='No Practices To Show !!'/>}
+        {userStories.length > 0 ? (
+          userStories.map((story) => (
+            <UserStory
+              key={story._id}
+              title={story.title}
+              contestTitle={story.contest?.contestTheme || ""}
+              promptTitle={story.prompt?.title}
+              prompt_id={story.prompt?._id}
+              description={story.content}
+              id={story._id}
+              corrections={story.corrections}
+              type={story.storyType}
+              contributors={story.contributors}
+              onsuccess={onSuccess}
+            />
+          ))
+        ) : (
+          <NotFound text="No Practices To Show !!" />
+        )}
       </div>
       {pageDetails && pageDetails.total > 5 && (
-              <div className="w-full ms-28">
-                <ReactPaginate
-                  previousLabel={
-                    <FaAngleLeft className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10" />
-                  }
-                  nextLabel={
-                    <FaAngleRight className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10" />
-                  }
-                  breakLabel="..."
-                  breakClassName="break-me"
-                  pageCount={Math.ceil(pageDetails.total / pageDetails.perPage)}
-                  marginPagesDisplayed={2}
-                  pageRangeDisplayed={5}
-                  onPageChange={handlePageClick}
-                  containerClassName="flex justify-center gap-2 md:gap-4 lg:gap-6 rounded-full mt-8"
-                  pageClassName=""
-                  pageLinkClassName="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 flex items-center justify-center border border-gray-300 rounded-full"
-                  previousClassName=""
-                  previousLinkClassName="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 flex items-center justify-center border border-gray-300 rounded-full"
-                  nextClassName=""
-                  nextLinkClassName="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 flex items-center justify-center border border-gray-300 rounded-full"
-                  activeClassName="bg-black text-white rounded-full"
-                />
-              </div>
-            )}
+        <div className="w-full ms-28">
+          <ReactPaginate
+            previousLabel={
+              <FaAngleLeft className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10" />
+            }
+            nextLabel={
+              <FaAngleRight className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10" />
+            }
+            breakLabel="..."
+            breakClassName="break-me"
+            pageCount={Math.ceil(pageDetails.total / pageDetails.perPage)}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageClick}
+            containerClassName="flex justify-center gap-2 md:gap-4 lg:gap-6 rounded-full mt-8"
+            pageClassName=""
+            pageLinkClassName="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 flex items-center justify-center border border-gray-300 rounded-full"
+            previousClassName=""
+            previousLinkClassName="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 flex items-center justify-center border border-gray-300 rounded-full"
+            nextClassName=""
+            nextLinkClassName="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 flex items-center justify-center border border-gray-300 rounded-full"
+            activeClassName="bg-black text-white rounded-full"
+          />
+        </div>
+      )}
     </div>
   );
 };
