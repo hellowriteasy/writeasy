63332b111b33f5885d66e643d18b0b5e2e04d58b
Prashant Bhusal <165927638+Parsantdeveloper@users.noreply.github.com>
2024-07-28 21:52:28 +0545

    admin practice error fixed

--- frontend/app/admin/practices/page.tsx
+++ frontend/app/admin/practices/page.tsx
@@ -3,17 +3,12 @@ import { useState, useEffect } from "react";
 import Card from "../../components/admin/practice/CardAdd";
 import Modal from "../../components/admin/practice/Modal";
 import ProtectedRoute from "@/app/utils/ProtectedRoute";
-import { TPrompt, TPageDetails } from "@/app/utils/types";
+import { TPrompt } from "@/app/utils/types";
 import { axiosInstance } from "@/app/utils/config/axios";
-import ReactPaginate from "react-paginate";
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
 const Page = () => {
-  const itemsPerPage = 5; // Adjust the items per page as needed
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [prompts, setPrompts] = useState<TPrompt[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalItems, setTotalItems] = useState(0);
   const AxiosIns = axiosInstance("");
   const [refetch, setRefetch] = useState(false);
 
@@ -21,31 +16,26 @@ const Page = () => {
     setIsModalOpen(true);
   };
 
-  const fetchPrompts = async (page = 1, perPage = itemsPerPage) => {
+  const fetchPrompts = async () => {
     try {
       const response = await AxiosIns.get("/prompts/practice-prompts", {
         params: {
-          page,
-          perPage,
+          page: 1,
+          perPage: 1000, // Set a high number to fetch all items
         },
       });
       setPrompts(response.data.data);
-      setTotalItems(response.data?.pageData?.total); // Adjust according to your API response
     } catch (error) {
       console.error("Error fetching prompts:", error);
     }
   };
 
   useEffect(() => {
-    fetchPrompts(currentPage, itemsPerPage); // Pages are 1-indexed in the API
-  }, [currentPage, refetch]);
+    fetchPrompts();
+  }, [refetch]);
 
   const onSuccess = () => {
     setRefetch(!refetch);
-  };
-
-  const handlePageClick = (event: { selected: number }) => {
-    setCurrentPage(event.selected + 1);
   };
 
   return (
@@ -86,32 +76,6 @@ const Page = () => {
                   />
                 ))}
               </div>
-              {totalItems > itemsPerPage && (
-                <div className="mt-4">
-                  <ReactPaginate
-                    previousLabel={
-                      <FaAngleLeft className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10" />
-                    }
-                    nextLabel={
-                      <FaAngleRight className="w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10" />
-                    }
-                    breakLabel="..."
-                    breakClassName="break-me"
-                    pageCount={Math.ceil(totalItems / itemsPerPage)}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={5}
-                    onPageChange={handlePageClick}
-                    containerClassName="flex justify-center gap-2 md:gap-4 lg:gap-6 rounded-full mt-8"
-                    pageClassName=""
-                    pageLinkClassName="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 flex items-center justify-center border border-gray-300 rounded-full"
-                    previousClassName=""
-                    previousLinkClassName="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 flex items-center justify-center border border-gray-300 rounded-full"
-                    nextClassName=""
-                    nextLinkClassName="w-8 h-8 md:w-12 md:h-12 lg:w-16 lg:h-16 flex items-center justify-center border border-gray-300 rounded-full"
-                    activeClassName="bg-black text-white rounded-full"
-                  />
-                </div>
-              )}
             </div>
           </div>
           {isModalOpen && (
--- frontend/app/components/admin/practice/Modal.tsx
+++ frontend/app/components/admin/practice/Modal.tsx
@@ -1,9 +1,8 @@
-import { Fragment, useState,useEffect } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import { Dialog, Transition, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
-import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Card from '../../../components/admin/contests/CardAdd'; // Import your Card component
+import Card from '../../../components/admin/contests/CardAdd';
 import { axiosInstance } from '@/app/utils/config/axios';
 
 interface ModalProps {
@@ -15,16 +14,17 @@ interface CardProps {
   title: string;
   type: string;
 }
-interface Category{
-  _id:string,
-  name:string
+
+interface Category {
+  _id: string;
+  name: string;
 }
 
-const Modal: React.FC<ModalProps> = ({ setIsModalOpen,onSuccess }) => {
+const Modal: React.FC<ModalProps> = ({ setIsModalOpen, onSuccess }) => {
   const [promptTitle, setPromptTitle] = useState('');
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]); // State for selected categories
   const [promptCards, setPromptCards] = useState<CardProps[]>([]);
-  const [category,setCategory]=useState<Category[]> ([]) // State to store added prompt cards
+  const [category, setCategory] = useState<Category[]>([]) // State to store added prompt cards
   const AxiosIns = axiosInstance('');
 
   const handleAdd = () => {
@@ -58,6 +58,7 @@ const Modal: React.FC<ModalProps> = ({ setIsModalOpen,onSuccess }) => {
       setSelectedCategories([...selectedCategories, category]);
     }
   };
+
   useEffect(() => {
     const getCategory = async () => {
       try {
@@ -70,6 +71,7 @@ const Modal: React.FC<ModalProps> = ({ setIsModalOpen,onSuccess }) => {
 
     getCategory();
   }, []);
+
   return (
     <Transition.Root show={true} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={() => setIsModalOpen(false)}>
@@ -86,7 +88,7 @@ const Modal: React.FC<ModalProps> = ({ setIsModalOpen,onSuccess }) => {
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -96,7 +98,7 @@ const Modal: React.FC<ModalProps> = ({ setIsModalOpen,onSuccess }) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full h-full sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
@@ -151,7 +153,7 @@ const Modal: React.FC<ModalProps> = ({ setIsModalOpen,onSuccess }) => {
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm  sm:ml-3 sm:w-auto"
+                    className="inline-flex w-full justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto"
                     onClick={handleAdd}
                   >
                     Add
