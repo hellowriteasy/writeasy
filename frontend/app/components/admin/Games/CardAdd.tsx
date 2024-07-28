abf6405a78723e932b280f2b567bef00d0b2ddfa
Prashant Bhusal <165927638+Parsantdeveloper@users.noreply.github.com>
2024-07-28 22:30:19 +0545

    other modal ui fixed

--- frontend/app/components/admin/Games/CardAdd.tsx
+++ frontend/app/components/admin/Games/CardAdd.tsx
@@ -96,7 +96,7 @@ const CardAdd: React.FC<CardProps> = ({
           </Transition.Child>
 
           <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -106,7 +106,7 @@ const CardAdd: React.FC<CardProps> = ({
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all w-1/2 sm:my-8 sm:w-full sm:max-w-lg">
                   <div className="bg-white h-auto px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                     <div className="sm:flex sm:items-start">
                       <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
