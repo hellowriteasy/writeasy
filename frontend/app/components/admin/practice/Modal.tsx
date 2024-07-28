9f9c9bb004ab4ee884be7e3181cf2133f35722f4
Prashant Bhusal <165927638+Parsantdeveloper@users.noreply.github.com>
2024-07-28 22:24:08 +0545

    fixed practice

--- frontend/app/components/admin/practice/Modal.tsx
+++ frontend/app/components/admin/practice/Modal.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState, useEffect } from 'react';
+import { Fragment, useState,useEffect } from 'react';
 import { Dialog, Transition, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -14,17 +14,16 @@ interface CardProps {
   title: string;
   type: string;
 }
-
-interface Category {
-  _id: string;
-  name: string;
+interface Category{
+  _id:string,
+  name:string
 }
 
-const Modal: React.FC<ModalProps> = ({ setIsModalOpen, onSuccess }) => {
+const Modal: React.FC<ModalProps> = ({ setIsModalOpen,onSuccess }) => {
   const [promptTitle, setPromptTitle] = useState('');
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]); // State for selected categories
   const [promptCards, setPromptCards] = useState<CardProps[]>([]);
-  const [category, setCategory] = useState<Category[]>([]) // State to store added prompt cards
+  const [category,setCategory]=useState<Category[]> ([]) // State to store added prompt cards
   const AxiosIns = axiosInstance('');
 
   const handleAdd = () => {
@@ -58,7 +57,6 @@ const Modal: React.FC<ModalProps> = ({ setIsModalOpen, onSuccess }) => {
       setSelectedCategories([...selectedCategories, category]);
     }
   };
-
   useEffect(() => {
     const getCategory = async () => {
       try {
@@ -71,7 +69,6 @@ const Modal: React.FC<ModalProps> = ({ setIsModalOpen, onSuccess }) => {
 
     getCategory();
   }, []);
-
   return (
     <Transition.Root show={true} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={() => setIsModalOpen(false)}>
@@ -84,11 +81,11 @@ const Modal: React.FC<ModalProps> = ({ setIsModalOpen, onSuccess }) => {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0  bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -98,12 +95,12 @@ const Modal: React.FC<ModalProps> = ({ setIsModalOpen, onSuccess }) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full h-full sm:w-full sm:max-w-lg">
+              <Dialog.Panel className="relative flex flex-col  items-center transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-1/2 h-full sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                        Edit Prompt
+                        Add Prompt
                       </Dialog.Title>
                       <div className="mt-2">
                         <input
@@ -150,10 +147,10 @@ const Modal: React.FC<ModalProps> = ({ setIsModalOpen, onSuccess }) => {
                     </div>
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <div className="bg-gray-50 px-4 py-3  sm:flex sm:flex-row-reverse  sm:px-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto"
+                    className="inline-flex w-full justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm  sm:ml-3 sm:w-auto"
                     onClick={handleAdd}
                   >
                     Add
