6d27e911d0462c8d469815a89d46f8fca946a247
baijanaththaru-skillprompt <santoshkunwar@skillprompt.com>
2024-09-18 20:25:19 +0545

    trigeering github

--- backend/app.js
+++ backend/app.js
@@ -72,5 +72,5 @@ app.use(errorHandlingMiddleware);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}...`);
 });
