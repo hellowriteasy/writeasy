791262c32b455b5237adab194690c005bb0ea184
baijanaththaru-skillprompt <santoshkunwar@skillprompt.com>
2024-09-21 08:47:41 +0545

    setuped network for redis and backend communication

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -2,7 +2,7 @@ const redis = require("redis");
 
 // Create Redis client
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379", // default Redis URL
+  url: "redis://redis:6379", // Use 'redis' as the hostname
 });
 
 redisClient.on("error", (err) => {
