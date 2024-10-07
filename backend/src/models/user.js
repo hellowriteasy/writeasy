7d9f20dd42409f37df5d946d70ebff527d5eb3e4
baijanaththaru-skillprompt <santoshkunwar@skillprompt.com>
2024-10-07 19:20:12 +0545

    added timestamps in user model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,24 +1,29 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: {
-    type: String,
-    required: function () {
-      return !this.googleId;
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: {
+      type: String,
+      required: function () {
+        return !this.googleId;
+      },
+    }, // Make password required only if googleId isn't set
+    googleId: { type: String, unique: true, sparse: true }, // For users registering through Google
+    profile_picture: { type: String },
+    subscriptionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Subscription",
     },
-  }, // Make password required only if googleId isn't set
-  googleId: { type: String, unique: true, sparse: true }, // For users registering through Google
-  profile_picture: { type: String },
-  subscriptionId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Subscription",
+    lastLogin: { type: Date, default: Date.now },
+    email_unsubscribed: { type: Boolean, default: false },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
   },
-  lastLogin: { type: Date, default: Date.now },
-  email_unsubscribed: { type: Boolean, default: false },
-  role: { type: String, enum: ["user", "admin"], default: "user" },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
